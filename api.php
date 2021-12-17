fd3c837d7bc30bbf807502a4dd57cccd8316d737
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-17 10:45:17 -0500

    21.12-dev-79

--- api.php
+++ api.php
@@ -279,7 +279,7 @@ class conversationsAPI extends CRUDAPI {
 							}
 						}
 					}
-          $conversation['files'] = trim(implode(";",$conversation['files']),';');
+          $conversation['files'] = implode(";",trim($conversation['files'],';'));
           $conversation['id'] = $this->saveConversation($conversation,$messages);
           $query = $this->Auth->query('UPDATE `messages` SET `isAttached` = ? WHERE `id` = ?',["true",$message["id"]])->dump();
         }
