ef954192173f4bbad2e2adc6dd690d26ed2cb167
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-28 16:21:22 -0500

    21.12-dev-106

--- dist/js/script.js
+++ dist/js/script.js
@@ -156,7 +156,7 @@ API.Plugins.conversations = {
 							html += '<i class="fas fa-'+defaults.icon+' bg-'+defaults.color+'"></i>';
 							html += '<div class="timeline-item">';
 								html += '<span class="time"><i class="fas fa-clock mr-2"></i><time class="timeago" datetime="'+dataset.created.replace(/ /g, "T")+'">'+dataset.created+'</time></span>';
-								html += '<h3 class="timeline-header border-0">'+dataset.account+' was linked</h3>';
+								html += '<h3 class="timeline-header border-0">['+dataset.account+'] Conversation '+dataset.id+' was linked</h3>';
 							html += '</div>';
 						html += '</div>';
 						layout.timeline.find('div.time-label[data-dateus="'+dateUS+'"]').after(html);
