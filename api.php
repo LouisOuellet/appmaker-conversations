dd4b6db5adb80f8985fd26a6e5cb335d49b7249a
Louis Ouellet <louis@TheMacSwitch.local>
2022-02-03 14:20:45 -0500

    22.02-dev-156

--- api.php
+++ api.php
@@ -301,7 +301,7 @@ class conversationsAPI extends CRUDAPI {
   protected function parseReference($string){
 		// Sanitize reference
 		$reference = $string;
-    $replace = ['CNTR-','PARS-','CCN:','VENDOR#','BILLING#','CROSSING#','LOCATION#','SBRN#','CCN#','PKG#','WEIGHT#','CLIENT#','CN:','CN#','TR#','OTHER:','PO:','PO#','MWB:','MWB#','STATUS#','REF:','NBR:','INV:','INV#','OTHER:','---','--','CID:','UTF-8','(',')','<','>','{','}','[',']',';','"',"'",'#','_','=','+','!','?','@','$','%','^','&','*','\\','/','|'];
+    $replace = ['CNTR-','PARS-','CCN:','VENDOR#','BILLING#','CROSSING#','LOCATION#','SBRN#','CCN#','PKG#','WEIGHT#','CLIENT#','CN:','CN#','TR#','OTHER:','PO:','PO#','MWB:','MWB#','STATUS#','REF:','NBR:','INV:','INV#','OTHER:','---','--','CID:','UTF-8','(',')','<','>','{','}','[',']',';','"',"'","~",'#','_','=','+','!','?','@','$','%','^','&','*','\\','/','|'];
     foreach($replace as $str1){ $reference = str_replace($str1,'',strtoupper($reference)); }
 		// Return based on string
 		if(substr($string, 0, 5) == "CNTR-" || substr($string, 0, 3) == "CN:" || substr($string, 0, 3) == "CN#"){
